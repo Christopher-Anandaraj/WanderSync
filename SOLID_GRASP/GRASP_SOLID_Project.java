f4fd05e0cee0c3ebe1f04af8f96ba265bba750b3
killerpawzz <littleblueally@gmail.com>
2024-10-13 22:20:39 -0400

    Started implementation for tasks.

--- SOLID_GRASP/GRASP_SOLID_Project.java
+++ SOLID_GRASP/GRASP_SOLID_Project.java
@@ -1,1 +1,52 @@
-//new java file for project classes
+//task interface
+public interface Task {
+    public void performTask();
+}
+
+//in projectTasks
+//in wantedTasks
+public class addTask implements Task {
+    private String title;
+    private String description;
+    private String dueDate;
+    private String status;
+    private String priority;
+
+    public addTask(String title, String description, String dueDate,
+                   String status, String priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    @Override
+    public void performTask() {
+        //what do I put here??
+    }
+
+}
+
+public class runTask implements Task {
+    private String title;
+    private String description;
+    private String dueDate;
+    private String status;
+    private String priority;
+
+    public runTask(String title, String description, String dueDate,
+                   String status, String priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    @Override
+    public void performTask() {
+        //what do I put here??
+    }
+
+}
