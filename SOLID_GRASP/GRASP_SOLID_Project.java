087fcd8c6c9aa286629fbbe7cc8f6fa1809be574
killerpawzz <littleblueally@gmail.com>
2024-10-14 21:19:14 -0400

    Built up SOLID principles in tasks

--- SOLID_GRASP/GRASP_SOLID_Project.java
+++ SOLID_GRASP/GRASP_SOLID_Project.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
+
 //task interface
-public interface Task {
+public interface taskManagement {
     public void performTask();
 }
 
 //in projectTasks
 //in wantedTasks
-public class addTask implements Task {
+public class addTask implements taskManagement {
     private String title;
     private String description;
     private String dueDate;
     private String status;
     private String priority;
 
+    //create the task
     public addTask(String title, String description, String dueDate,
                    String status, String priority) {
         this.title = title;
@@ -21,14 +24,16 @@ public class addTask implements Task {
         this.priority = priority;
     }
 
+    //add task to project arrayList
     @Override
     public void performTask() {
-        //what do I put here??
+        projectTasks.add(this); //project tasks arraylist should be named this
+        wantedTask.add(this) //added to wantedtask array?
     }
 
 }
 
-public class runTask implements Task {
+public class runTask implements taskManagement {
     private String title;
     private String description;
     private String dueDate;
@@ -44,9 +49,22 @@ public class runTask implements Task {
         this.priority = priority;
     }
 
+    //mark task as complete
     @Override
     public void performTask() {
-        //what do I put here??
+        this.status = "Complete";
     }
+
+}
+
+public interface task {
+    void performTask();
+}
+
+public class genericTask implements task {
+
+}
+
+public class specializedTask extends genericTask {
 
 }
