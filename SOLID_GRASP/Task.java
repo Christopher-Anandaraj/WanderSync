db0b675703beb900aaeb4e7a0ce65d387ce62075
Xavistin Christopher Arul Arasu <61247123+Christopher-Anandaraj@users.noreply.github.com>
2024-10-19 13:44:07 -0400

    Added runTask method to all the sub tasks.java

--- SOLID_GRASP/Task.java
+++ SOLID_GRASP/Task.java
@@ -5,71 +5,76 @@ public abstract class Task {
     protected String status;
     protected String priority;
     protected String assignedTo;
+    protected String message;
 
-    @Override
     public void setTitle(String title) {
         this.title = title;
     }
 
-    @Override
     public String getTitle() {
         return title;
     }
 
-    @Override
     public void setDescription(String description) {
         this.description = description;
     }
 
-    @Override
     public String getDescription() {
         return description;
     }
 
-    @Override
     public void setDueDate(String dueDate) {
         this.dueDate = dueDate;
     }
 
-    @Override
     public String getDueDate() {
         return dueDate;
     }
 
-    @Override
+
     public void setPriority(String priority) {
         this.priority = priority;
     }
 
-    @Override
     public String getPriority() {
         return priority;
     }
 
-    @Override
     public void setStatus(String status) {
         this.status = status;
     }
 
-    @Override
     public String getStatus() {
         return status;
     }
 
-    @Override
     public void setAssignedTo(String assignedTo) {
         this.assignedTo = assignedTo;
     }
 
-    @Override
     public String getAssignedTo() {
         return assignedTo;
     }
 
-    @Override
     public void setStatusComplete() {
         this.status = "Complete";
     }
+
+    public void runTask() {
+    }
+}
+
+//Generic class
+class GenericTask extends Task {
+    public GenericTask(String title, String description, String status) {
+        setTitle(title);
+        setDescription(description);
+        setStatus(status);
+    }
+    @Override
+        public void runTask() {
+            System.out.println("This is a generic task!");
+        }
 }
 
 //an example special task that uses parent Task
@@ -80,7 +85,12 @@ class PriorityTask extends Task {
         setPriority(priority);
         setStatus(status);
     }
+    @Override
+        public void runTask() {
+            System.out.println("This is a priority task!");
+        }
 }
+
 //another example
 class DueDateTask extends Task {
     public DueDateTask(String title, String description, String dueDate, String status) {
@@ -89,7 +99,12 @@ class DueDateTask extends Task {
         setDueDate(dueDate);
         setStatus(status);
     }
+    @Override
+        public void runTask() {
+            System.out.println("This is a due date task!");
+        }
 }
+
 //ANOTHER example I think you get it by now
 class AssignedTask extends Task {
     public AssignedTask(String title, String description, String assignedTo, String status) {
@@ -98,4 +113,8 @@ class AssignedTask extends Task {
         setAssignedTo(assignedTo);
         setStatus(status);
     }
+    @Override
+        public void runTask() {
+            System.out.println("This is a assigned task!");
+        }
 }
