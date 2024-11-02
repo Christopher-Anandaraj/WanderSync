4df57e53acca68b38a132b8c9f034b34fea2268d
vhern026 <vhern026@gmail.com>
2024-11-02 16:06:20 -0400

    Resolving merge issues

---
+++ Code_Smells/src/Customer.java
@@ -0,0 +1,28 @@
+//added new clas due to bloater/large class smells violation in order
+
+public class Customer {
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getCustomerName() {
+        return name;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.name = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return email;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.email = customerEmail;
+    }
+
+}
