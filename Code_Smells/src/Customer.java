3b71d566d1cfe1171bc6d6033e50e514b8f115ca
Allyson M Martinez <161779504+killerPawzz@users.noreply.github.com>
2024-11-02 14:19:42 -0400

    Create Customer.java
    added customer to avoid bloated class in order.

---
+++ Code_Smells/src/Customer.java
@@ -0,0 +1,22 @@
+//added new clas due to bloater/large class smells violation in order
+
+public class Customer {
+    String customerName;
+    String customerEmail;
+}
+
+public String getCustomerName() {
+    return customerName;
+}
+
+public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+}
+
+public String getCustomerEmail() {
+    return customerEmail;
+}
+
+public void setCustomerEmail(String customerEmail) {
+    this.customerEmail = customerEmail;
+}
